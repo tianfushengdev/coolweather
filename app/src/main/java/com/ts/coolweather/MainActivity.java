1cb4327ee71ca9a8ce0c94352b60cc3cf2318a93
ts <tian05184054@163.com>
2017-05-03 11:17:42 +0800

    完成遍历省市县三级列表功能

--- app/src/main/java/com/ts/coolweather/MainActivity.java
+++ app/src/main/java/com/ts/coolweather/MainActivity.java
@@ -1,13 +1,22 @@
 package com.ts.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+//            Intent intent = new Intent(this, WeatherActivity.class);
+//            startActivity(intent);
+            finish();
+        }
+    }
+
 }
