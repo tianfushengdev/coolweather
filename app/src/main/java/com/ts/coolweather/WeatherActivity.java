58943ce7a8e845624aed6796f711b612dbf9781e
ts <tian05184054@163.com>
2017-05-03 15:27:00 +0800

    添加后台自动更新天气功能、修改图标和名称

--- app/src/main/java/com/ts/coolweather/WeatherActivity.java
+++ app/src/main/java/com/ts/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.ts.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.ts.coolweather.gson.Forecast;
 import com.ts.coolweather.gson.Weather;
+import com.ts.coolweather.service.AutoUpdateService;
 import com.ts.coolweather.util.HttpUtil;
 import com.ts.coolweather.util.Utility;
 
@@ -229,8 +231,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
-//        Intent intent = new Intent(this, AutoUpdateService.class);
-//        startService(intent);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
